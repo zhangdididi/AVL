f57df43db90fae954a07755db1101af97d2e00c9
zhangdididi <49708573+zhangdididi@users.noreply.github.com>
2020-07-05 18:37:20 +0800

    Create main.java

---
+++ main.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Random random = new Random(20200705);
+        AVLTree tree = new AVLTree();
+
+        for (int i = 0; i < 1000; i++) {
+            int r = random.nextInt(10000);
+            try {
+                tree.insert(r);
+            } catch (RuntimeException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        tree.verify();
+    }
+}
